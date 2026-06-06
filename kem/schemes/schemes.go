9b643e0cdbed1c091cb06c9bbd189290293b1b1e
blakehartin <bh@bh>
2026-06-05 18:32:23 -0700

    remove unused schemes part 3

--- kem/schemes/schemes.go
+++ kem/schemes/schemes.go
@@ -2,53 +2,30 @@
 //
 // # Schemes Implemented
 //
-// Based on standard elliptic curves:
-//
-//	HPKE_KEM_P256_HKDF_SHA256, HPKE_KEM_P384_HKDF_SHA384, HPKE_KEM_P521_HKDF_SHA512
-//
-// Based on standard Diffie-Hellman functions:
-//
-//	HPKE_KEM_X25519_HKDF_SHA256, HPKE_KEM_X448_HKDF_SHA512
-//
 // Post-quantum kems:
 //
-//	Kyber512, Kyber768, Kyber1024
+//	ML-KEM-512, ML-KEM-768, ML-KEM-1024
+//
+// Hybrid kems:
+//
+//	X25519MLKEM768
 package schemes
 
 import (
 	"strings"
 
-	"github.com/quantumcoinproject/circl/hpke"
 	"github.com/quantumcoinproject/circl/kem"
 	"github.com/quantumcoinproject/circl/kem/hybrid"
-	"github.com/quantumcoinproject/circl/kem/kyber/kyber1024"
-	"github.com/quantumcoinproject/circl/kem/kyber/kyber512"
-	"github.com/quantumcoinproject/circl/kem/kyber/kyber768"
 	"github.com/quantumcoinproject/circl/kem/mlkem/mlkem1024"
 	"github.com/quantumcoinproject/circl/kem/mlkem/mlkem512"
 	"github.com/quantumcoinproject/circl/kem/mlkem/mlkem768"
-	"github.com/quantumcoinproject/circl/kem/xwing"
 )
 
 var allSchemes = [...]kem.Scheme{
-	hpke.KEM_P256_HKDF_SHA256.Scheme(),
-	hpke.KEM_P384_HKDF_SHA384.Scheme(),
-	hpke.KEM_P521_HKDF_SHA512.Scheme(),
-	hpke.KEM_X25519_HKDF_SHA256.Scheme(),
-	hpke.KEM_X448_HKDF_SHA512.Scheme(),
-	kyber512.Scheme(),
-	kyber768.Scheme(),
-	kyber1024.Scheme(),
 	mlkem512.Scheme(),
 	mlkem768.Scheme(),
 	mlkem1024.Scheme(),
-	hybrid.Kyber512X25519(),
-	hybrid.Kyber768X25519(),
-	hybrid.Kyber768X448(),
-	hybrid.Kyber1024X448(),
-	hybrid.P256Kyber768Draft00(),
 	hybrid.X25519MLKEM768(),
-	xwing.Scheme(),
 }
 
 var allSchemeNames map[string]kem.Scheme
